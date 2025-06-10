3bd48b0025babf3c7ac1e73f40ff4ebc88be062d
Luis Herrera <u202218227@upc.edu.pe>
2025-06-09 19:40:42 -0500

    chore: fixed errors in Collaborate

--- Services/Collaborate/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Services/Collaborate/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+900b62530acfd208abb1b0cb67d30998a71c4e29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81e004515ae3073d3a4daa3de069289b6bd283a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/Profiles/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Services/Profiles/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+900b62530acfd208abb1b0cb67d30998a71c4e29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81063c20f23b755aff6bfa049195206ff745612b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
